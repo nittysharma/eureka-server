8a683a72a92c16eb3277a07ec59c4869ee7f4c16
Nitty Sharma <gaccount4m@gmail.com>
2022-01-03 21:58:02 +0530

    committing code for eureka

--- src/main/java/com/ibm/microservices/eurekaserver/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/ibm/microservices/eurekaserver/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,6 @@ package com.ibm.microservices.eurekaserver.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
